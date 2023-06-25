ae2114f176c095bb5835e1fce7ff4c5fc44c2e88
yavuz <yavuzucarkus9@gmail.com>
2023-06-26 00:32:06 +0300

    last commit

--- final_project/router/general.js
+++ final_project/router/general.js
@@ -29,21 +29,108 @@ public_users.get('/', function (req, res) {
     return res.send(JSON.stringify(books, null, 4))
 });
 
+//async wait usage
+// public_users.get('/', async function (req, res) {
+//     try {
+//       const books = await getAllBooks(); // Call an asynchronous function to get all the books
+//       return res.send(JSON.stringify(books, null, 4));
+//     } catch (error) {
+//       console.error(error);
+//       return res.status(500).send('Server Error');
+//     }
+//   });
+  
+//   // Asynchronous function to retrieve all books
+//   async function getAllBooks() {
+//     return new Promise((resolve, reject) => {
+//       // Perform the necessary operations to fetch the books here
+//       // For example, you can retrieve the books from a database or an external API
+  
+//       // Resolve the promise with the retrieved books when the operation is complete
+//       resolve(books);
+//     });
+// }
+
 // Get book details based on ISBN
 public_users.get('/isbn/:isbn', function (req, res) {
-    //Write your code here
-    const isbn = parseInt(req.params.isbn)
-    return res.send(books[isbn])
-});
+        //Write your code here
+        const isbn = parseInt(req.params.isbn)
+        return res.send(books[isbn])
+    });
+
+
+// using Promise
+// public_users.get('/isbn/:isbn', function (req, res) {
+//     const isbn = parseInt(req.params.isbn);
+
+//     getBookByIsbn(isbn)
+//         .then(book => {
+//             return res.send(book);
+//         })
+//         .catch(error => {
+//             console.error(error);
+//             return res.status(500).send('Server Error');
+//         });
+// });
+
+// function getBookByIsbn(isbn) {
+//     return new Promise((resolve, reject) => {
+//         if (isbn in books) {
+//             resolve(books[isbn]);
+//         } else {
+//             reject(new Error('Kitap bulunamadı'));
+//         }
+//     });
+// }
+
 
 // Get book details based on author
 public_users.get('/author/:author', function (req, res) {
     //Write your code here
     const author = req.params.author
     let authorBooks = Object.values(books).filter((book) => book.author === author);
-    let booksbyAuthor = {"booksbyAuthor": authorBooks}
+    let booksbyAuthor = { "booksbyAuthor": authorBooks }
     return res.send(booksbyAuthor);
 });
+
+// using Promise
+// // Get book details based on author
+// public_users.get('/author/:author', function (req, res) {
+//     const author = req.params.author;
+//     getBooksByAuthor(author)
+//         .then(books => {
+//             const booksByAuthor = { "booksByAuthor": books };
+//             return res.send(booksByAuthor);
+//         })
+//         .catch(error => {
+//             console.error(error);
+//             return res.status(500).send('Server Error');
+//         });
+// });
+
+// function getBooksByAuthor(author) {
+//     return new Promise((resolve, reject) => {
+//         const authorBooks = Object.values(books).filter(book => book.author === author);
+//         resolve(authorBooks);
+//     });
+// }
+
+
+//  Get book review
+public_users.get('/review/:isbn', function (req, res) {
+    //Write your code here
+    const isbn = req.params.isbn
+
+    if (books[isbn]) {
+        return res.send(books[isbn].reviews);
+    }
+    else {
+        return res.status(404).json({ message: "book not found" });
+    }
+});
+
+
+
 
 // Get all books based on title
 public_users.get('/title/:title', function (req, res) {
@@ -53,17 +140,26 @@ public_users.get('/title/:title', function (req, res) {
     return res.send(titleBasedBooks);
 });
 
-//  Get book review
-public_users.get('/review/:isbn', function (req, res) {
-    //Write your code here
-    const isbn = req.params.isbn
 
-    if(books[isbn]){
-        return res.send(books[isbn].reviews);
-    }
-    else{
-        return res.status(404).json({message: "book not found"}); 
-    }
-});
+// using Promise
+// // Get all books based on title
+// public_users.get('/title/:title', function (req, res) {
+//     const title = req.params.title;
+//     getBooksByTitle(title)
+//         .then(books => {
+//             return res.send(books);
+//         })
+//         .catch(error => {
+//             console.error(error);
+//             return res.status(500).send('Server Error');
+//         });
+// });
+
+// function getBooksByTitle(title) {
+//     return new Promise((resolve, reject) => {
+//         const titleBasedBooks = Object.values(books).filter(book => book.title === title);
+//         resolve(titleBasedBooks);
+//     });
+// }
 
 module.exports.general = public_users;
